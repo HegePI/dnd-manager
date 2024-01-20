df2c9194df019d91d858098d2cc844591e87f1fc
Heikki Pulli <heikki.pulli@helsinki.fi>
2024-01-20 15:31:25 +0200

    🛠️ dnd-manager.code-workspace -> Added "system-tests" path 🛠️ dnd-manager.code-workspace -> Updated VS Code extensions 🟢 system-tests/.gitignore -> New file added 🟢 system-tests/package-lock.json -> New file added 🟢 system-tests/package.json -> New file added 🟢 system-tests/playwright.config.ts -> New file added 🟢 system-tests/tests/auth.setup.ts -> New file added 🟢 system-tests/tests/frontpage-visible.spec.ts -> New file added

---
+++ system-tests/tests/auth.setup.ts
@@ -0,0 +1,14 @@
+import { test as setup, expect } from "@playwright/test";
+
+const authFile = "playwright/.auth/user.json";
+
+setup("authenticate", async ({ page }) => {
+  await page.goto("http://dnd-manager.local/");
+  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.getByPlaceholder("jsmith").fill("tester");
+  await page.getByLabel("Password").fill("tester");
+  await page.getByRole("button", { name: "Sign in with Credentials" }).click();
+  await expect(page.getByText("Signed in as tester Sign out")).toBeVisible();
+
+  await page.context().storageState({ path: authFile });
+});
